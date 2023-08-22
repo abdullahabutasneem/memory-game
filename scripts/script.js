3757cc07e30ed3706847800c278bcf63aa45160a
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2023-08-23 01:12:33 +0600

    creating game grid

---
+++ scripts/script.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // card options
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'cheeseburger',
+            img: 'images/cheeseburger.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+    ]
+
+    const grid = document.getElementById('game-grid');
+    console.log("akib");
+    // creating a game board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img');
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('data-id', i);
+            console.log(i);
+            // card.addEventListener('click', flipCard);
+            grid.appendChild(card);
+        }
+    }
+
+    createBoard();
+})
+
